2f8069d6173da744627e798a2f166660d20045d6
IvanLeschinsky <ivan.leschinsky@yandex.ru>
2019-01-16 12:38:20 +0500

    Initial commit

---
+++ desktop/src/wanl/example/com/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package wanl.example.com.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import wanl.example.com.SpaceShooter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceShooter(), config);
+	}
+}
