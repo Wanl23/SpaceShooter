5e2ed7c1c3a597956f23cdbc159f0cdc1154b662
IvanLeschinsky <ivan.leschinsky@yandex.ru>
2019-01-22 19:25:43 +0500

    HW3

--- core/src/wanl/example/com/base/Base2dScreen.java
+++ core/src/wanl/example/com/base/Base2dScreen.java
@@ -3,41 +3,87 @@ package wanl.example.com.base;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector2;
+
+import wanl.example.com.math.MatrixUtils;
+import wanl.example.com.math.Rect;
 
 public class Base2dScreen implements Screen, InputProcessor {
+
+    protected SpriteBatch batch;
+
+    private Rect screenBounds;
+    private Rect worldBounds;
+    private Rect glBounds;
+
+    private Matrix4 worldToGl;
+    private Matrix3 screenToWorld;
+
+    protected Vector2 touch;
+    protected Vector2 buf;
+    protected Vector2 position;
+    protected Vector2 v;
+    private static final float CONST_LENGTH = 0.01f;
+
     @Override
     public void show() {
+
+        batch = new SpriteBatch();
         Gdx.input.setInputProcessor(this) ;
+        this.screenBounds = new Rect();
+        this.worldBounds = new Rect();
+        this.glBounds = new Rect(0, 0, 1f, 1f);
+
+        this.worldToGl = new Matrix4();
+
+        touch = new Vector2();
+        this.screenToWorld = new Matrix3();
+        position = new Vector2(-0.5f, -0.5f);
+        buf = new Vector2(0, 0);
+        v = new Vector2();
     }
 
     @Override
     public void render(float delta) {
+        buf.set(touch);
+        if (buf.sub(position).len() > CONST_LENGTH) {
+            position.add(v);
+        } else {
+            position.set(touch);
+        }
 
     }
 
     @Override
     public void resize(int width, int height) {
+        screenBounds.setSize(width, height);
+        screenBounds.setLeft(0);
+        screenBounds.setBottom(0);
 
+        float aspect = width / (float) height;
+        worldBounds.setHeight(1f);
+        worldBounds.setWidth(1f * aspect);
+
+        MatrixUtils.calcTransitionMatrix(worldToGl, worldBounds, glBounds);
+        batch.setProjectionMatrix(worldToGl);
+        MatrixUtils.calcTransitionMatrix(screenToWorld, screenBounds, worldBounds);
     }
 
     @Override
-    public void pause() {
-
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-
-    }
+    public void resume() {}
 
     @Override
-    public void hide() {
-
-    }
+    public void hide() {}
 
     @Override
     public void dispose() {
-
+        batch.dispose();
     }
 
     @Override
@@ -57,16 +103,37 @@ public class Base2dScreen implements Screen, InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        v.set(touch.cpy().sub(position).setLength(CONST_LENGTH));
+        touchDown(touch, pointer);
+        return false;
+    }
+
+    public boolean touchDown(Vector2 touch, int pointer) {
+        System.out.println(new Vector2(touch.x, touch.y).len());
         return false;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+
+        touchUp(touch, pointer);
+        return false;
+    }
+
+    public boolean touchUp(Vector2 touch, int pointer) {
         return false;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchDragged(touch, pointer);
+        return false;
+    }
+
+    public boolean touchDragged(Vector2 tousch, int pointer) {
         return false;
     }
 
--- core/src/wanl/example/com/screen/MenuScreen.java
+++ core/src/wanl/example/com/screen/MenuScreen.java
@@ -10,27 +10,17 @@ import com.badlogic.gdx.math.Vector2;
 import wanl.example.com.base.Base2dScreen;
 
 public class MenuScreen extends Base2dScreen {
-    SpriteBatch batch;
+
     Texture img;
     Texture background;
-    Vector2 position;
-    Vector2 speed;
-    Vector2 tochDownPosition;
-    Vector2 buf;
-    private static final float V_LEN = 10.5f;
+
+
 
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
         img = new Texture("badlogic.jpg");
-        batch.getProjectionMatrix().idt();
         background = new Texture("spaceBack.jpg");
-        position = new Vector2(0, 0);
-        speed = new Vector2(0,0);
-        tochDownPosition = new Vector2(0, 0);
-        buf = new Vector2(0, 0);
-
     }
 
     @Override
@@ -39,32 +29,24 @@ public class MenuScreen extends Base2dScreen {
         Gdx.gl.glClearColor(1, 1, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(background, 0, 0);
-        batch.draw(img, -1f, -1f, 2f, 2f);
+        batch.draw(background, -0.5f, -0.5f, 1f, 1f);
+        batch.draw(img, position.x, position.y, 0.5f, 0.5f);
         batch.end();
-        buf.set(tochDownPosition);
-        if (buf.sub(position).len() > V_LEN) {
-            position.add(speed);
-        } else {
-            position.set(tochDownPosition);
-        }
-//        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) position.x -= 200 * Gdx.graphics.getDeltaTime();
-//        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) position.x += 200 * Gdx.graphics.getDeltaTime();
-//        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) position.y -= 200 * Gdx.graphics.getDeltaTime();
-//        if(Gdx.input.isKeyPressed(Input.Keys.UP)) position.y += 200 * Gdx.graphics.getDeltaTime();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
         img.dispose();
         super.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        tochDownPosition.set(screenX, Gdx.graphics.getHeight() - screenY);
-        speed.set(tochDownPosition.cpy().sub(position).setLength(V_LEN));
-        return super.touchDown(screenX, screenY, pointer, button);
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        return super.touchDown(touch, pointer);
     }
 }
